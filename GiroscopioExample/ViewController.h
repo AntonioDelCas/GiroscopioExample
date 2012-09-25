72cb3f83102dc839f06f46697132268230b53bd3
AntonioDelCas <antoniojdelgado@yahoo.es>
2012-09-25 15:32:26 +0200

    Prueba de subida a GitHub
    Prueba de subida y sincronización desde Numenti

--- GiroscopioExample/ViewController.h
+++ GiroscopioExample/ViewController.h
@@ -7,7 +7,48 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
 
 @interface ViewController : UIViewController
+{
+    /**Variable que proporciona los servicios motion*/
+    CMMotionManager *motionManager;
+    /**Variable para establecer el tiempo de actualización*/
+    NSTimer *timer;
+    /**variable que corresponde a la rotación del dispositivo*/
+    float rotation;
+
+}
+
+
+/**Propiedad correspondiente a la imagen del avión*/
+@property(nonatomic, strong) IBOutlet UIImageView *imagen;
+
+/**Propiedad correspondiente la etiqueta de la rotación en X*/
+@property(nonatomic, strong) IBOutlet UILabel *x;
+
+/**Propiedad correspondiente la etiqueta de la rotación en Y*/
+@property(nonatomic, strong) IBOutlet UILabel *y;
+
+/**Propiedad correspondiente la etiqueta de la rotación en Z*/
+@property(nonatomic, strong) IBOutlet UILabel *z;
+
+/**
+ * Método llamado cuando se cambia el switch para habilitar
+ * o deshabilitar las actualizaciones del giroscopio *
+ * @param sender botón que envio la llamada *
+ */
+-(IBAction)cambioActualizaciones:(id)sender;
+
+/**
+ * Método para actualizacion del giro *
+ */
+-(void)doGyroUpdate;
+
+/**
+ * Método para mostrar el valor de la rotacion del dispositivo *
+ */
+-(void)setGyro;
+
 
 @end
--- GiroscopioExample/ViewController.m
+++ GiroscopioExample/ViewController.m
@@ -8,27 +8,123 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+//@interface ViewController ()
 
-@end
-
+//@end
 @implementation ViewController
+
+@synthesize imagen, x, y, z;
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+#pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    /*Se inicializa la variable motionManager*/
+    motionManager = [[CMMotionManager alloc] init];
 }
 
-- (void)viewDidUnload
-{
+- (void)viewDidUnload {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
+    // Release any retained subviews of the main view. // e.g. self.myOutlet = nil;
 }
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
+
+#pragma mark - Métodos auxiliares
+-(IBAction)cambioActualizaciones:(id)sender {
+    /*si el boton está en ON*/
+    if ([sender isOn]) {
+        /*se comienzan a capturar actualizaciones del giroscopio*/
+        [motionManager startGyroUpdates];
+        /*se especifica el intervalo de actualización*/
+        timer = [NSTimer scheduledTimerWithTimeInterval:1/30.0
+                                                 target:self
+                                               selector:@selector(doGyroUpdate)
+                                               userInfo:nil
+                                                repeats:YES];
+                 }
+    /*si el boton está en OFF*/
+    else
+    {
+        /*se termina la captura actualizaciones del giroscopio*/
+        [motionManager stopGyroUpdates];
+        /*se para el timer*/
+        [timer invalidate];
+        /*se pone el texto de los labels vacios*/ x.text = @"";
+        y.text = @"";
+        z.text = @"";
+    }
+}
+  
+
+-(void)doGyroUpdate {
+    /*se recoge la rotación en Z*/
+    float rate = motionManager.gyroData.rotationRate.z; /*si el valor absoluto de rate es mayor que 0.2*/
+    if (fabs(rate) > .2) {
+            /*se declara una variable local direction*/
+            float direction;
+            /*si rate es mayor que 0*/
+            if(rate>0)
+            {
+                /*direction toma el valor 1*/
+                direction=1;
+            }
+                /*sino*/
+            else
+            {
+                direction=-1;
+            }
+            /*se incrementa el valor de rotation en +PI/90 o -PI/90 dependiendo*/
+        rotation += direction * M_PI/90.0; /*se realiza la rotacion en la imagen*/
+        self.imagen.transform = CGAffineTransformMakeRotation(rotation);
+    }
+    [self setGyro];
+}
+
+
+
+
+-(void)setGyro {
+    /*se situa en la etiqueta X el valor de la coordenada X*/
+    x.text = [[NSString alloc]initWithFormat:@"%f",motionManager.gyroData.rotationRate.x];
+    /*se situa en la etiqueta Y el valor de la coordenada Y*/
+    y.text = [[NSString alloc]initWithFormat:@"%f",motionManager.gyroData.rotationRate.y];
+    /*se situa en la etiqueta Z el valor de la coordenada Z*/
+    z.text = [[NSString alloc]initWithFormat:@"%f",motionManager.gyroData.rotationRate.z];
 }
 
 @end
